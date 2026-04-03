55f76df9606b349d6ec6edd63f169e39dfdd0776
Rick van der Wolk <rickvanderwolk@gmail.com>
2026-04-03 10:18:14 +0200

    Show number of coffee cups used for training after model is saved

--- train_model.py
+++ train_model.py
@@ -9,6 +9,8 @@ data = data[data['coffee_brewed'].isin(['yes', 'no'])]
 
 data['coffee_brewed'] = data['coffee_brewed'].map({'yes': 1, 'no': 0})
 
+coffee_count = int((data['coffee_brewed'].diff() == 1).sum())
+
 data['mean_last_15'] = data['power'].rolling(window=15).mean()
 data['max_last_15'] = data['power'].rolling(window=15).max()
 data['min_last_15'] = data['power'].rolling(window=15).min()
@@ -22,6 +24,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
-
 joblib.dump(model, 'coffee_model.pkl')
-print("Trained model has been saved as 'coffee_model.pkl'.")
+print(f"Trained model has been saved as 'coffee_model.pkl' (trained on {coffee_count} cups of coffee).")
